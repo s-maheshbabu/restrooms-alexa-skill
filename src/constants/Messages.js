b6e70230f64495ab266694cd61c41f7074896d9a
Mahesh Sabbavarapu <s.maheshbabu@hotmail.com>
2020-08-25 00:42:07 -0700

    Improve the missing permissions messages to be more verbose.

--- src/constants/Messages.js
+++ src/constants/Messages.js
@@ -1,6 +1,6 @@
 const messages = Object.freeze({
-    NOTIFY_MISSING_GEO_LOCATION_PERMISSIONS: 'Please enable geo location permissions in the Amazon Alexa app.',
-    NOTIFY_MISSING_DEVICE_ADDRESS_PERMISSIONS: 'Please enable device address permissions in the Amazon Alexa app.',
+    NOTIFY_MISSING_GEO_LOCATION_PERMISSIONS: 'I need permissions to access your device address to search for restrooms near by. I put a card in the Amazon Alexa app to make it easy for you. Please open the Alexa app and enable device address permissions.',
+    NOTIFY_MISSING_DEVICE_ADDRESS_PERMISSIONS: 'I need permissions to access your device address to search for restrooms near by. I put a card in the Amazon Alexa app to make it easy for you. Please open the Alexa app and enable device address permissions.',
 });
 
 module.exports = {
